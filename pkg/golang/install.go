bf029328a3f9536d1b032ed8ca32c5a5a9b27799
Nikhita Raghunath <nikitaraghunath@gmail.com>
2019-11-11 15:32:06 +0530

    Bump default Go version to 1.13.4

--- pkg/golang/install.go
+++ pkg/golang/install.go
@@ -29,7 +29,7 @@ import (
 	"k8s.io/publishing-bot/cmd/publishing-bot/config"
 )
 
-const defaultGoVersion = "1.12.9"
+const defaultGoVersion = "1.13.4"
 
 // installGoVersions download and unpacks the specified Golang versions to $GOPATH/
 func InstallDefaultGoVersion() error {
