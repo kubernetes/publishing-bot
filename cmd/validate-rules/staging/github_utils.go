3dabf20550a50e645e35c8484f315933dbc9ccad
Davanum Srinivas <davanum@gmail.com>
2021-12-04 07:35:45 -0500

    Check if the directories are actually present in k8s branches
    Signed-off-by: Davanum Srinivas <davanum@gmail.com>

---
+++ cmd/validate-rules/staging/github_utils.go
@@ -0,0 +1,82 @@
+/*
+Copyright 2021 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package staging
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+type File struct {
+	Name string `json:"name"`
+	Path string `json:"path"`
+	Type string `json:"type"`
+}
+
+// fetchKubernetesStagingDirectoryFiles uses the GH API to get the contents
+// of the contents/staging/src/k8s.io directory in a specified branch of kubernetes
+func fetchKubernetesStagingDirectoryFiles(branch string) ([]File, error) {
+	url := "https://api.github.com/repos/kubernetes/kubernetes/contents/staging/src/k8s.io?ref=" + branch
+
+	spaceClient := http.Client{}
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var res *http.Response
+	count := 0
+	for {
+		res, err = spaceClient.Do(req)
+		if err != nil {
+			return nil, err
+		}
+
+		if res.Body != nil {
+			defer res.Body.Close()
+		}
+
+		if res.StatusCode == http.StatusForbidden {
+			// try after some time as we hit GH API limit
+			time.Sleep(5 * time.Second)
+			count++
+		} else {
+			// try for 10 mins then give up!
+			if count == 120 {
+				return nil, fmt.Errorf("hitting github API limits, bailing out")
+			} else {
+				break
+			}
+		}
+	}
+
+	body, readErr := ioutil.ReadAll(res.Body)
+	if readErr != nil {
+		return nil, readErr
+	}
+
+	var result []File
+	jsonErr := json.Unmarshal(body, &result)
+	if jsonErr != nil {
+		return nil, jsonErr
+	}
+
+	return result, nil
+}
---
+++ cmd/validate-rules/staging/validate.go
@@ -0,0 +1,76 @@
+/*
+Copyright 2021 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package staging
+
+import (
+	"fmt"
+	"github.com/golang/glog"
+	"k8s.io/publishing-bot/cmd/publishing-bot/config"
+	"path/filepath"
+)
+
+// globalMapBranchDirectories is a cache to avoid hitting GH limits
+// key is the branch (`master` or `release-1.23`) and the value
+// is the list of files/directories fetched using GH api in the
+// correct directory
+var globalMapBranchDirectories = make(map[string][]File)
+
+// EnsureStagingDirectoriesExist walks through the repository rules and checks
+// if the specified directories are present in the specific kubernetes branch
+func EnsureStagingDirectoriesExist(rules *config.RepositoryRules) []error {
+	glog.Infof("validating directories exist in the kubernetes branch")
+
+	var errors []error
+	for _, rule := range rules.Rules {
+		for _, branchRule := range rule.Branches {
+			_, directory := filepath.Split(branchRule.Source.Dir)
+			err := checkDirectoryExistsInBranch(directory, branchRule.Source.Branch)
+			if err != nil {
+				errors = append(errors, err)
+			}
+			for _, dependency := range branchRule.Dependencies {
+				err := checkDirectoryExistsInBranch(dependency.Repository, dependency.Branch)
+				if err != nil {
+					errors = append(errors, err)
+				}
+			}
+		}
+	}
+	return errors
+}
+
+func checkDirectoryExistsInBranch(directory string, branch string) error {
+	// Look in the cache first
+	files, ok := globalMapBranchDirectories[branch]
+	if !ok {
+		var err error
+		files, err = fetchKubernetesStagingDirectoryFiles(branch)
+		if err != nil {
+			globalMapBranchDirectories[branch] = []File{}
+			return fmt.Errorf("error fetching directories from branch %s : %w", branch, err)
+		}
+		globalMapBranchDirectories[branch] = files
+	}
+
+	for _, file := range files {
+		// check the name and that it is a directory!
+		if file.Name == directory && file.Type == "dir" {
+			return nil
+		}
+	}
+	return fmt.Errorf("%s not found in branch %s", directory, branch)
+}
